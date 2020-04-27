bc2cc0616f1b42448a5b88752f8c93b47871d1e5
PAPATIRI <syariftaufik11@gmail.com>
2020-04-27 19:13:48 +0700

    call the content of API

--- weather/lookup/views.py
+++ weather/lookup/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
+# todo api kode : http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=5&API_KEY=9CBA3349-C777-42CC-BC87-86791526BB68
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+
+    api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=5&API_KEY=9CBA3349-C777-42CC-BC87-86791526BB68")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "error"
+
+    return render(request, 'home.html', {'api':api})
 
 
 def about(request):
